763e00677fc72a67063f20da850728a4cd443b70
kitcnya <kitcnya@outlook.com>
2023-11-11 14:50:47 +0900

    more simple key monitor

--- source.c
+++ source.c
@@ -16,20 +16,9 @@
  * - https://docs.qmk.fm/#/feature_macros
  */
 
-#define MAXTIMER	60000		/* msec */
-static uint16_t basetime = 0;
-
 void
 housekeeping_task_user(void)
 {
-#ifdef CONSOLE_ENABLE
-	if (debug_enable) {
-		uint16_t time = timer_elapsed(basetime);
-		if (time < MAXTIMER) return;
-		basetime += time;
-		print("LAPPED\n");
-	}
-#endif
 }
 
 bool
@@ -37,8 +26,10 @@ process_record_user(uint16_t keycode, keyrecord_t *record)
 {
 #ifdef CONSOLE_ENABLE
 	if (debug_enable) {
-		uint16_t time = timer_elapsed(basetime);
-		uprintf("%05u %04X %s\n", time, keycode, (record->event.pressed ? "p" : "r"));
+		uprintf("%05u %04X %02u %02u %u\n",
+			record->event.time, keycode,
+			record->event.key.col, record->event.key.row,
+			record->event.pressed);
 	}
 #endif
 	return true;
