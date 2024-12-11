ecfde334c74b3595a105244d877f4b1ef5252a67
kitcnya <kitcnya@outlook.com>
2024-12-12 08:07:57 +0900

    add alternative keymap 2 and 3 for test

--- config.h
+++ config.h
@@ -11,3 +11,4 @@
 #define ML2323	QK_USER_1
 #define ML3323	QK_USER_2
 #define ML3131	QK_USER_3
+#define ML4242	QK_USER_4
--- source.c
+++ source.c
@@ -19,7 +19,7 @@
 #endif /* LAYER_AUTO_OFF_TIMEOUT */
 
 #ifndef LAYER_AUTO_OFF_LAYER
-#define LAYER_AUTO_OFF_LAYER	3	/* lowest layer number to manage */
+#define LAYER_AUTO_OFF_LAYER	4	/* lowest layer number to manage */
 #endif /* LAYER_AUTO_OFF_LAYER */
 
 static uint32_t layer_auto_off_action_timer = 0;
@@ -132,10 +132,11 @@ static struct multi_tap_or_hold_def {
 	bool pending;			/* timer pending action exists */
 	enum multi_tap_or_hold_state state;
 } multi_tap_or_hold[] = {
-	MTHDEF(ML2222, 2, 2, 2, 2),
-	MTHDEF(ML2323, 2, 3, 2, 3),
-	MTHDEF(ML3323, 3, 3, 2, 3),
-	MTHDEF(ML3131, 3, 1, 3, 1),
+//	MTHDEF(ML2222, 2, 2, 2, 2),
+//	MTHDEF(ML2323, 2, 3, 2, 3),
+//	MTHDEF(ML3323, 3, 3, 2, 3),
+//	MTHDEF(ML3131, 3, 1, 3, 1),
+	MTHDEF(ML4242, 4, 2, 4, 2),
 };
 
 #define NMTHDEFS (sizeof(multi_tap_or_hold) / sizeof(struct multi_tap_or_hold_def))
